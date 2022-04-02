07d09b8de2699068bf9a1559dc329ceebebe6705
fikarsuwardi <fikarsuwardi@gmail.com>
2022-04-02 12:21:32 +0800

    release0

--- models/class.js
+++ models/class.js
@@ -1,1 +1,34 @@
 "use strict"
+
+class Plane {
+    constructor (flightNumber, airlineName, origin, destinantion) {
+        this.flightNumber = flightNumber;
+        this.airlineName = airlineName;
+        this.origin = origin;
+        this.destinantion = destinantion;
+    }
+}
+
+class Passenger {
+    constructor (id, name, gender, ticket,) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.ticket = ticket;
+    }
+}
+
+class Ticket {
+    #seatNumber;
+    constructor (airlineName, type, origin, destinantion, seatNumber) {
+        this.airlineName = airlineName;
+        this.type = type;
+        this.origin = origin;
+        this.destinantion = destinantion;
+        this.#seatNumber = seatNumber;
+    } 
+}
+
+class Factory {
+    static 
+}
